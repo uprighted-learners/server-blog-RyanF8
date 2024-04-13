d58a77e908b22c6f9d1b3216b4a72cb1b8625dce
RyanF8 <Ryanfish4229@gmail.com>
2024-04-13 18:19:09 -0400

    all endpoints are completed and tested, everything seems to be working good, project is done, will have it checked out and make any changes needed

--- controllers/routes.js
+++ controllers/routes.js
@@ -3,31 +3,31 @@ const { read, save } = require('../readWrite.js');
 const dbPath = './api/blog.json';
 
 router.get('/api/test', (req, res) => { //this is a test 
-    res.send('were up and going')
+    res.send('were up and going');
 });
 
 //get all from db - /get/all
 router.get('/get/all', (req, res) => {
     try {
-        const allPosts = read(dbPath)
-        res.status(200).json(allPosts)
+        const allPosts = read(dbPath);
+        res.status(200).json(allPosts);
     } catch (error) {
-        res.status(500).json({message: 'error getting all posts'})
+        res.status(500).json({message: 'error getting all posts'});
     }
 });
 
 //get one db by post_id - /get/one/:id
 router.get('/get/one/:id', (req, res) => {
     try {
-        const postId = req.params.id
-        const allPosts = read(dbPath)
-        const onePost = allPosts.find(post => post.post_id.toString() === postId)//get the whole post, stringify the id because its comparing a number to a string, get the id that matches the params 
+        const postId = req.params.id;
+        const allPosts = read(dbPath);
+        const onePost = allPosts.find(post => post.post_id.toString() === postId);//get the whole post, stringify the id because its comparing a number to a string, get the id that matches the params 
         
-        if (!onePost) res.status(404).json({message: 'id does not exist'}) //error if the id does not exist 
+        if (!onePost) res.status(404).json({message: 'id does not exist'}); //error if the id does not exist 
     
-        res.status(200).json(onePost)
+        res.status(200).json(onePost);
     } catch (error) {
-        res.status(500).json({message: 'error getting one post'})//error 
+        res.status(500).json({message: 'error getting one post'});//error 
     }
 });
 
@@ -43,10 +43,10 @@ router.post('/create', (req,res) => {
             body
         }
         allPosts.push(post) //add new post to all posts and save it 
-        save(allPosts, dbPath)
-        res.status(200).json(post)
+        save(allPosts, dbPath);
+        res.status(200).json(post);
     } catch (error) {
-        res.status(500).json({message: 'error creating new post'})
+        res.status(500).json({message: 'error creating new post'});
     }
 });
 
@@ -61,7 +61,7 @@ router.put('/update/:id', (req, res) => {
         if (postIndex === -1) { //if post index is not found then error
             res.status(404).json({ message: 'Post not found' });
             return;
-        }
+        };
         allPosts[postIndex] = {  // Update the post with the new data
             ...allPosts[postIndex], //seperate the array into individual elements(used this syntax in zorkington)
             title,
@@ -77,6 +77,27 @@ router.put('/update/:id', (req, res) => {
 });
 
 //delete - /delete/:id - make sure to tell user it has been deleted
+router.delete('/delete/:id', (req, res) => {
+    try {
+        const postId = req.params.id;
+        let allPosts = read(dbPath); 
+
+        
+        const postIndex = allPosts.findIndex(post => post.post_id.toString() === postId);// Find the index of the post with the given postId
+
+        if (postIndex === -1) { // If post not found, return 404
+            res.status(404).json({ message: 'Post not found' });
+            return;
+        }
+        const deletedPost = allPosts.splice(postIndex, 1);// Remove the post from the array
+
+        save(allPosts, dbPath);
+        res.status(200).json({ message: 'Post deleted', deletedPost });//notify the user and show the post deleted
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Error deleting post' });
+    }
+});
 
 
 
