1853cdc30d367bb6b3cf916d425d0ea2bbe011f6
RyanF8 <Ryanfish4229@gmail.com>
2024-04-13 17:08:11 -0400

    put endpoint works, just need a delete endpoint

--- controllers/routes.js
+++ controllers/routes.js
@@ -23,9 +23,8 @@ router.get('/get/one/:id', (req, res) => {
         const allPosts = read(dbPath)
         const onePost = allPosts.find(post => post.post_id.toString() === postId)//get the whole post, stringify the id because its comparing a number to a string, get the id that matches the params 
         
-        if (!onePost) {
-            res.status(404).json({message: 'id does not exist'}) //error if the id does not exist 
-        }
+        if (!onePost) res.status(404).json({message: 'id does not exist'}) //error if the id does not exist 
+    
         res.status(200).json(onePost)
     } catch (error) {
         res.status(500).json({message: 'error getting one post'})//error 
@@ -48,21 +47,40 @@ router.post('/create', (req,res) => {
         res.status(200).json(post)
     } catch (error) {
         res.status(500).json({message: 'error creating new post'})
-        console.log(error)
     }
 });
 
-//put - /update/:id - if no data is given for any item do not update the item
+// put - /update/:id - if no data is given for any item do not update the item
+router.put('/update/:id', (req, res) => {
+    try {
+        const postId = req.params.id;
+        const { title, author, body } = req.body;
+        const allPosts = read(dbPath);
+        const postIndex = allPosts.findIndex(post => post.post_id.toString() === postId);// Find the index of the post with the given postId
+
+        if (postIndex === -1) { //if post index is not found then error
+            res.status(404).json({ message: 'Post not found' });
+            return;
+        }
+        allPosts[postIndex] = {  // Update the post with the new data
+            ...allPosts[postIndex], //seperate the array into individual elements(used this syntax in zorkington)
+            title,
+            author,
+            body
+        };
+        save(allPosts, dbPath); // Save the updated list of posts
+        res.status(200).json(allPosts[postIndex]);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Error updating post' });
+    }
+});
 
 //delete - /delete/:id - make sure to tell user it has been deleted
+
+
 
 module.exports = router;
 
 
 //start link with localhost:5500/blog AND THEN add endpoint 
-// newPost = {
-//     "post_id": 2,
-//     "title": "Second Blog Post",
-//     "author": "Ryan Fish",
-//     "body": "We all ask how much the wood chuck chucked, but never how the chucker of wood is doing"
-// }
